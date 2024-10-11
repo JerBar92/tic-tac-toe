8daeb9f574559555047f32e09a91422614c5afbd
JerBar92 <174113693+JerBar92@users.noreply.github.com>
2024-10-11 12:30:50 +0200

    Final commit

--- gameBoard.js
+++ gameBoard.js
@@ -1,19 +1,20 @@
-import { playRound } from "./playRound.js";
-
 export const gameBoard = (function () {
   let board = ["", "", "", "", "", "", "", "", ""];
   const player1 = document.querySelector("#player1");
   const player2 = document.querySelector("#player2");
   const playerTurn = document.querySelector("#playerTurn");
+  const endMessage = document.querySelector("#endMessage");
 
   let getBoard = () => board;
 
+  //link between the board array and the DOM
   const update = (index, value) => {
     let cell = document.getElementById(`${index}`);
     board[index] = value;
     cell.textContent = value;
   };
 
+  // reset all elements
   const reset = () => {
     for (let index = 0; index < board.length; index++) {
       board[index] = "";
@@ -23,7 +24,30 @@ export const gameBoard = (function () {
     player1.value = "";
     player2.value = "";
     playerTurn.textContent = "";
+    endMessage.textContent = "";
   };
 
-  return { getBoard, update, reset };
+  const checkVictory = (board) => {
+    const winningCases = [
+      [[0], [1], [2]],
+      [[3], [4], [5]],
+      [[6], [7], [8]],
+      [[0], [3], [6]],
+      [[1], [4], [7]],
+      [[2], [5], [8]],
+      [[0], [4], [8]],
+      [[2], [4], [6]],
+    ];
+    for (let i = 0; i < winningCases.length; i++) {
+      const [a, b, c] = winningCases[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return true;
+      }
+    }
+  };
+  const checkTie = (board) => {
+    return board.every((cell) => cell !== "");
+  };
+
+  return { getBoard, update, reset, checkVictory, checkTie };
 })();
--- playRound.js
+++ playRound.js
@@ -1,19 +1,18 @@
 import { gameBoard } from "./gameBoard.js";
-import { victory } from "./victory.js";
 
 export const playRound = (function () {
   let players = [];
   let activePlayer;
   let gameOn;
 
+  //display the name of current player
   const printNewRound = () => {
     const displayActivePlayer = document.querySelector("#playerTurn");
     displayActivePlayer.textContent = "";
     displayActivePlayer.textContent = `${activePlayer.name}'s turn.`;
   };
 
-  const getActivePlayer = () => activePlayer;
-
+  //initialize game
   const start = () => {
     players = [
       { name: document.querySelector("#player1").value, mark: "O" },
@@ -30,13 +29,32 @@ export const playRound = (function () {
     });
   };
 
+  //display the result of the game
+  const displayResult = () => {
+    const endMessage = document.querySelector("#endMessage");
+    if (gameBoard.checkVictory(gameBoard.getBoard())) {
+      endMessage.textContent = `${activePlayer.name} win !`;
+    } else if (gameBoard.checkTie(gameBoard.getBoard())) {
+      endMessage.textContent = "It's a tie";
+    }
+  };
+
+  //game controller
   const handleClick = (cell) => {
     let index = parseInt(cell.id);
     if (gameBoard.getBoard()[index] !== "") return;
     gameBoard.update(index, activePlayer.mark);
-    //victory.checkVictory();
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    printNewRound();
+    if (gameBoard.checkVictory(gameBoard.getBoard())) {
+      gameOn = false;
+    } else if (gameBoard.checkTie(gameBoard.getBoard())) {
+      gameOn = false;
+    }
+    if (gameOn === false) {
+      displayResult();
+    } else {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+      printNewRound();
+    }
   };
-  return { start, handleClick, getActivePlayer };
+  return { start, handleClick };
 })();
